458a495769bc494cb3470a570af9a5afa1ce9dbe
comstudy21 <comstudy21@naver.com>
2020-12-17 10:18:48 +0900

    new inc func

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -3,4 +3,6 @@ package sig.oss;
 public class Calc {
 	public int add(int x, int y) { return x+y; }
 	public int sub(int x, int y) { return x-y; }
+	public int inc(int d) { return d+1; } // 작업중...
 }
+///
