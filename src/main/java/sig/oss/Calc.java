5d6f84a5469b78129f79120ab4e6ea3ca35bc15d
comstudy21joon <76138542+comstudy21joon@users.noreply.github.com>
2020-12-17 10:57:41 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,8 +1,12 @@
 package sig.oss;
 
 public class Calc {
+	public int r = 0;
 	public int add(int x, int y) { return x+y; }
 	public int sub(int x, int y) { return x-y; }
-	public int inc(int d) { return d+1; } // 작업중...
+	public int inc(int d) { 
+		r += d;
+		return r; 
+	} // 작업중...
 }
 ///
